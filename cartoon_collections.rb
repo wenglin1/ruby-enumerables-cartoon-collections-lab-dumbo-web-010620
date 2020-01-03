240ae6c72731047da8c804bf73632a7a2bdbdebc
weng lin <wenglin@buffalo.edu>
2020-01-03 18:37:22 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,8 @@ def summon_captain_planet(elements)
   x = elements.map {|name| "#{name.capitalize}!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(arr)
+  array.any?{|name| name.length > 4}
 end
 
 def find_the_cheese# code an argument here
