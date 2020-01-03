fd515643c0736b903c56f85f0d57ab2cd9bd31a6
weng lin <wenglin@buffalo.edu>
2020-01-03 18:35:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(elements)
-  elements.each {|name| puts "#{name}!"}
+  elements.each {|name| puts "#{name.capitalize}!"}
 end
 
 def long_planeteer_calls# code an argument here
